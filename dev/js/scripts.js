b25b07d516aeae93e3b885c8bc782baee17b9a95
Josh <josh@gdovindesigns.com>
2020-08-02 22:35:57 -0400

    added demo files

---
+++ dev/js/scripts.js
@@ -0,0 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+
+//gsap.to("#yellow-box",{duration:1, x:400, rotation: 360, scrollTrigger:"#yellow-box"});
+
+// Toggle Actions 
+
+gsap.to("#yellow-box",{
+    duration:1, 
+    x:400, 
+    rotation: 360, 
+    scrollTrigger:{
+        trigger: "#yellow-box",
+        // first property of top refers to the yellow box, the center property refers to the viewport
+        start: "top 60%",
+        markers: true,
+        scrub:1,
+        pin: true,
+        // onEnter, onLeave, onEnterBack, onLeaveBack
+        toggleActions: "restart none none none"
+    }});
+
+    // keywords
+    // play pause resume reverse restart reset complete none
